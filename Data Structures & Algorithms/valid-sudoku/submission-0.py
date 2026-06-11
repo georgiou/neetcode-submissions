0d247c5b5ee27f2da2fff66c84ccf26809fdb3e5
Kostas Georgiou <k.georgiou@gmail.com>
2026-06-11 13:28:09 +0100

    Add: valid-sudoku - submission-0

---
+++ Data Structures & Algorithms/valid-sudoku/submission-0.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        n = len(board[0])
+        seen = {}
+
+        for i in range(n):
+            for j in range(n):
+                v = board[i][j]
+                if v == '.':
+                    continue
+                if f"r{i}{v}" in seen:
+                    return False
+                seen[f"r{i}{v}"]=i
+
+                if f"c{j}{v}" in seen:
+                    return False
+                seen[f"c{j}{v}"]=j
+
+                if f"i{(i//3)*3+(j//3)}{v}" in seen:
+                    return False
+                seen[f"i{(i//3)*3+(j//3)}{v}"]=(i/3)*3+(j/3)
+        print(seen)
+        return True
